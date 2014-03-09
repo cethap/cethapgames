1f133b19669c797ddea9862c5f8a42422b29ff48
Cesar Augusto Tapasco <cethapgames@gmail.com>
2014-03-09 16:07:34 -0500

    arreglos al ojo

--- routes/eye.js
+++ routes/eye.js
@@ -5,16 +5,12 @@
 var fs = require('fs');
 exports.index = function(req, res){
   res.header("Access-Control-Allow-Origin", "*");
-	// var j1 = fs.readFileSync('routes/eye/1.js'),
-	// j2 = fs.readFileSync('routes/eye/2.js'),
-	// j3 = fs.readFileSync('routes/eye/3.js');
-	//res.render('eye', {j1:j1,j2:j2,j3:j3});
+
 res.writeHead(200, {'Content-Type': 'text/html'});
-res.write(fs.readFileSync('views/eye.html'));
+//res.write(fs.readFileSync('views/eye.html'));
+res.write(fs.readFileSync(__dirname+"/../views/eye.html"));
 res.end();
 
-  //res.end();
-  //res.render('eye', {});
 };
 
 exports.eyeUI = function(req, res){
